3057445541570cd2e188adb7fc42ba0465f486a5
Subhas Dandapani <rdsubhas@gmail.com>
2017-11-13 16:39:46 +0100

    Fix gofmt

--- core/bindata.go
+++ core/bindata.go
@@ -162,7 +162,7 @@ func AssetNames() []string {
 // _bindata is a table, holding each asset generator, mapped to its name.
 var _bindata = map[string]func() (*asset, error){
 	"gen/LICENSES": genLicenses,
-	"gen/version": genVersion,
+	"gen/version":  genVersion,
 }
 
 // AssetDir returns the file names below a certain
@@ -204,10 +204,11 @@ type bintree struct {
 	Func     func() (*asset, error)
 	Children map[string]*bintree
 }
+
 var _bintree = &bintree{nil, map[string]*bintree{
-	"gen": &bintree{nil, map[string]*bintree{
-		"LICENSES": &bintree{genLicenses, map[string]*bintree{}},
-		"version": &bintree{genVersion, map[string]*bintree{}},
+	"gen": {nil, map[string]*bintree{
+		"LICENSES": {genLicenses, map[string]*bintree{}},
+		"version":  {genVersion, map[string]*bintree{}},
 	}},
 }}
 
@@ -257,4 +258,3 @@ func _filePath(dir, name string) string {
 	cannonicalName := strings.Replace(name, "\\", "/", -1)
 	return filepath.Join(append([]string{dir}, strings.Split(cannonicalName, "/")...)...)
 }
-
