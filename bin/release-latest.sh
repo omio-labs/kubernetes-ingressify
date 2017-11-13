363eeec8fabe4229187bc6262acee095007e9a92
Subhas Dandapani <rdsubhas@gmail.com>
2017-11-13 14:59:32 +0100

    Release latest commit always

---
+++ bin/release-latest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -xe
+
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+  echo "Not on master branch, skipping latest release"
+  exit 0
+fi
+
+VERSION="v0.0.1-latest"
+git tag -d $VERSION
+git tag $VERSION
+git push https://${GH_TOKEN}:x-oauth-basic@github.com/goeuro/ingress-generator-kit.git $VERSION -f
