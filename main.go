02b538e78a7822a32332c8df6fc2a2f53f9c58a7
Subhas Dandapani (RDX) <rdsubhas@users.noreply.github.com>
2017-11-13 16:43:24 +0100

    Fix gofmt (#2)
    * Fail gofmt correctly
    
    * Fix gofmt
    
    * Add codecov.yml

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"github.com/apex/log"
-  "github.com/goeuro/ingress-generator-kit/core"
-  "strings"
+	"github.com/goeuro/ingress-generator-kit/core"
+	"strings"
 )
 
 func main() {
-  data, err := core.Asset("gen/version")
-  if err != nil {
-    log.WithError(err).Error("error")
-  }
+	data, err := core.Asset("gen/version")
+	if err != nil {
+		log.WithError(err).Error("error")
+	}
 
-  version := strings.TrimSpace(string(data))
+	version := strings.TrimSpace(string(data))
 	log.Infof("ingress-generator-kit version %s", version)
 }
