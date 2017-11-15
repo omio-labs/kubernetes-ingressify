8452c4b4f8c84802f7dbc34268298838416d30a9
Lorenzo Fundaró <lfundaro@users.noreply.github.com>
2017-11-15 14:19:48 +0100

    deps (#10)
    * deps
    
    * format file

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/apex/log"
 	"github.com/goeuro/kubernetes-ingressify/core"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	"strings"
 )
 
@@ -14,4 +19,29 @@ func main() {
 
 	version := strings.TrimSpace(string(data))
 	log.Infof("kubernetes-ingressify version %s", version)
+
+	var kubeconfig *string
+	kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	// in_template := flag.String("in_template", "", "path to template file used to render the config")
+	// out_file := flag.String("out-file", "", "path to save the resulting config after rendering template")
+	// interval := flag.String("interval", "", "poll interval")
+	// hook := flag.String("hook", "", "script to run after file is rendered")
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	ingressClient := clientset.ExtensionsV1beta1().Ingresses("")
+	list, err := ingressClient.List(metav1.ListOptions{})
+	for _, ing := range list.Items {
+		fmt.Printf(" * %s/%s %v \n", ing.Namespace, ing.Name, ing)
+	}
 }
