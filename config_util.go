de2f87f7944019c5b675bcba476ddd6cb3103d3d
Lorenzo Fundaro <lorenzo.fundaro@goeuro.com>
2017-11-22 19:12:18 +0100

    renamed config.go

---
+++ config_util.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/ghodss/yaml"
+	"io/ioutil"
+	"time"
+)
+
+type Config struct {
+	Kubeconfig  string `json:"kubeconfig"`
+	Interval    string `json:"interval"`
+	InTemplate  string `json:"in_template"`
+	OutTemplate string `json:"out_file"`
+	Hooks       Hook   `json:"hooks"`
+}
+
+func (c Config) GetInterval() (time.Duration, error) {
+	return time.ParseDuration(c.Interval)
+}
+
+func ReadConfig(path string) Config {
+	var config Config
+	dat, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err.Error())
+	}
+	err = yaml.Unmarshal(dat, &config)
+	if err != nil {
+		panic(err.Error())
+	}
+	return config
+}
