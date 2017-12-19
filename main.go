154e5f9fc784a0a83be07d47ee1e0edcf4e23bf2
Lorenzo Fundaro <lorenzo.fundaro@goeuro.com>
2017-12-19 15:31:09 +0100

    first draft on bubbling up renderstatus

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -27,7 +28,9 @@ func main() {
 
 	config := ReadConfig(*configPath)
 
-	go bootstrapHealthCheck(config.HealthCheckPort)
+	renderReport := make(chan RenderStatus, 10)
+
+	go bootstrapHealthCheck(config.HealthCheckPort, renderReport)
 
 	fmap := template.FuncMap{
 		"GroupByHost": GroupByHost,
@@ -53,17 +56,25 @@ func main() {
 	}
 
 	if *dryRun {
-		render(config, clientset, tmpl, false)
+		render(config, clientset, tmpl, false, renderReport)
 	} else {
 		for range time.NewTicker(duration).C {
-			render(config, clientset, tmpl, true)
+			render(config, clientset, tmpl, true, renderReport)
 		}
 	}
 }
 
-func bootstrapHealthCheck(port uint32) {
+func bootstrapHealthCheck(port uint32, hookStatus <-chan RenderStatus) {
+	lastReport := RenderStatus{isSuccess: true, error: nil}
 	http.HandleFunc("/health", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "I am alive !\n")
+		lastReport.mux.Lock()
+		select {
+		case lastReport = <-hookStatus:
+			createHealthResponse(lastReport, writer)
+		default:
+			createHealthResponse(lastReport, writer)
+		}
+		lastReport.mux.Unlock()
 	})
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
@@ -72,12 +83,32 @@ func bootstrapHealthCheck(port uint32) {
 	}
 }
 
-func render(config Config, clientset *kubernetes.Clientset, tmpl *template.Template, withHooks bool) {
+func createHealthResponse(lastReport RenderStatus, writer http.ResponseWriter) {
+	if lastReport.isSuccess {
+		fmt.Fprintf(writer, "Healthy !\n")
+		writer.WriteHeader(http.StatusOK)
+	} else {
+		fmt.Fprintf(writer, "Unhealthy: %s !\n", lastReport.error)
+		writer.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+// RenderStatus holds information to track failures/success on render function
+// this information gets bubbled up to the health check.
+type RenderStatus struct {
+	isSuccess bool
+	error     error
+	mux       sync.Mutex
+}
+
+func render(config Config, clientset *kubernetes.Clientset, tmpl *template.Template, withHooks bool,
+	renderReport chan<- RenderStatus) {
 	irules, err := ScrapeIngresses(clientset, "")
 	cxt := ICxt{IngRules: irules}
 	err = RenderTemplate(tmpl, config.OutTemplate, cxt)
 	if err != nil {
 		log.WithError(err).Error("Failed to render template")
+		renderReport <- RenderStatus{isSuccess: false, error: err}
 		return
 	}
 	if withHooks {
@@ -85,9 +116,11 @@ func render(config Config, clientset *kubernetes.Clientset, tmpl *template.Templ
 		out, err := ExecHook(config.Hooks.PostRender)
 		if err != nil {
 			log.WithError(err).Error("Failed to run post hook")
+			renderReport <- RenderStatus{isSuccess: false, error: err}
 			return
 		}
 		log.Info("Output from post hook")
 		fmt.Println(out)
+		renderReport <- RenderStatus{isSuccess: true, error: nil}
 	}
 }
